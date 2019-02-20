f590ba4e7e557e272135a632b8de359e761a4c5d
Niko Dyer <nikodyer44@gmail.com>
2019-02-19 23:32:13 -0700

    final commit

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -21,6 +21,25 @@ class NotesController < ApplicationController
     end
   end
 
+  def edit
+    @note = Note.find(params[:id])
+  end
+
+  def update
+    @note = Note.find(params[:id])
+
+    if @note.update(notes_params)
+      redirect_to notes_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    Note.find(params[:id]).destroy
+    redirect_to notes_path
+  end
+
   private
 
     def notes_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
+
+  get '/about', to: 'static_pages#about'
 
   resources :notes
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
